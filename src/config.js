9c5994522036f8f7d635cd814db4bb1ff4945124
CrystalBlockDev <crytal.blockdev@gmail.com>
2022-11-10 07:53:19 +0900

    building project creating page

--- src/config.js
+++ src/config.js
@@ -1,2 +1,9 @@
 export const INFURA_KEY = "1b4c44fdf5a0404b91ee1a85db0aed9a";
 export const BACKEND_URL = "http://localhost:5000";
+export const PROJECT_DOMAINS = [
+    "Education",
+    "Health",
+    "Environment",
+    "fight against counterfeiting",
+    "fight aginst food insufficiency"
+]
--- src/pages/project/add.js
+++ src/pages/project/add.js
@@ -1,10 +1,37 @@
 import {
-    useEffect
+    useEffect, useState
 } from "react";
+import { PROJECT_DOMAINS } from "../../config";
 
 const AddProject = () => {
-    useEffect(() => {
-    }, []);
+
+    const [title, setTitle] = useState("");
+    const [domain, setDomain] = useState("");
+    const [contactPersonLastName, setContactPersonLastName] = useState("");
+    const [contactPersonFirstName, setContactPersonFirstName] = useState("");
+    const [contactPersonTelephone, setContactPersonTelephone] = useState("");
+    const [contactPersonLinkedIn, setContactPersonLinkedIn] = useState("");
+    const [contactPersonEmail, setContactPersonEmail] = useState("");
+    const [keyPeoplesArray, setKeyPeoplesArray] = useState([]);
+    const [keyPeopleLastName, setKeyPeopleLastName] = useState("");
+    const [keyPeopleFirstName, setKeyPeopleFirstName] = useState("");
+    const [keyPeopleTelephone, setKeyPeopleTelephone] = useState("");
+    const [keyPeopleLinkedIn, setKeyPeopleLinkedIn] = useState("");
+    
+    const [refresh, setRefresh] = useState(false);
+
+    const onAddToKeyPeoplesArr =  () => {
+        let temp = keyPeoplesArray;
+        temp.push({
+            lastname: keyPeopleLastName,
+            firstname: keyPeopleFirstName,
+            telephone: keyPeopleTelephone,
+            linkedin: keyPeopleLinkedIn
+        });
+        console.log("temp = ", temp);
+        setKeyPeoplesArray(temp);
+        setRefresh(!refresh);
+    }
 
     return (
         <div className="grid w-11/12 px-3 pb-10 mx-auto sm:w-5/6 sm:px-10 md:px-16 lg:px-24 main-grid">
@@ -12,27 +39,23 @@ const AddProject = () => {
 
             <div className="mt-3 title-of-project">
                 <h2 className="mb-2 text-2xl font-bold leading-normal">Title of your project</h2>
-                <input type='text' className="input-default w-80 sm:w-96 sm:mr-5" />
+                <input type='text' className="input-default w-80 sm:w-96 sm:mr-5" 
+                    value={title} onChange={(e) => {setTitle(e.target.value)}}
+                />
             </div>
 
             <div className="mt-3 select-your-domain">
                 <h2 className="mb-2 text-2xl font-bold leading-normal">Select your domain</h2>
                 <div className="flex flex-wrap">
-                    <button type="button" className="mx-1 mb-2 rounded-full btn-default">
-                        Education
-                    </button>
-                    <button type="button" className="mx-1 mb-2 rounded-full btn-default">
-                        Health
-                    </button>
-                    <button type="button" className="mx-1 mb-2 rounded-full btn-default">
-                        Environment
-                    </button>
-                    <button type="button" className="mx-1 mb-2 rounded-full btn-default">
-                        fight against counterfeiting
-                    </button>
-                    <button type="button" className="mx-1 mb-2 rounded-full btn-default">
-                        fight against food insufficiency
-                    </button>
+                {
+                    PROJECT_DOMAINS.map((item, index) => (
+                        <button type="button" className="mx-1 mb-2 rounded-full btn-default" key={index}
+                            onClick={() => setDomain(item)}
+                        >
+                            {item}
+                        </button>
+                    ))
+                }
                 </div>
             </div>
             
@@ -41,33 +64,40 @@ const AddProject = () => {
                 <div className="flex flex-wrap">
                     <div className="flex-none input-group">
                         <div className="font-bold">Name</div>
-                        <input type="text" className="input-default sm:mr-5" />
+                        <input type="text" className="input-default sm:mr-5" value={contactPersonLastName} onChange={(e) => setContactPersonLastName(e.target.value)} />
                     </div>
                     <div className="flex-none input-group">
                         <div className="font-bold">First name</div>
-                        <input type="text" className="input-default sm:mr-5" />
+                        <input type="text" className="input-default sm:mr-5" value={contactPersonFirstName} onChange={(e) => setContactPersonFirstName(e.target.value)} />
                     </div>
                 </div>
                 <div className="flex flex-wrap">
                     <div className="flex-none input-group">
                         <div className="font-bold">Telephone</div>
-                        <input type="text" className="input-default w-80 sm:w-96 sm:mr-5" />
+                        <input type="text" className="input-default w-80 sm:w-96 sm:mr-5" value={contactPersonTelephone} onChange={(e) => setContactPersonTelephone(e.target.value)} />
                     </div>
                     <div className="flex-none input-group">
                         <div className="font-bold">LinkedIn</div>
-                        <input type="text" className="input-default w-80 sm:w-96 sm:mr-5" />
+                        <input type="text" className="input-default w-80 sm:w-96 sm:mr-5" value={contactPersonLinkedIn} onChange={(e) => setContactPersonLinkedIn(e.target.value)} />
                     </div>
                 </div>
                 <div className="flex">
                     <div className="flex-none input-group">
                         <div className="font-bold">Email</div>
-                        <input type="email" className="input-default w-80 sm:w-96 sm:mr-5" />
+                        <input type="email" className="input-default w-80 sm:w-96 sm:mr-5" value={contactPersonEmail} onChange={(e) => setContactPersonEmail(e.target.value)} />
                     </div>
                 </div>
             </div>
 
             <div className="mt-3 who-are-the-key">
                 <h2 className="mb-2 text-2xl font-bold leading-normal">Who are the key people in your business and what do they do?</h2>
+                {
+                    keyPeoplesArray.length>0 && keyPeoplesArray.map((item, index) => (
+                        <div key={index} >
+                            {item.lastname + ", " + item.firstname + ", " + item.telephone + ", " + item.linkedin}
+                        </div>
+                    ))
+                }
                 <div className="grid">
                     <div>
                         <i className="text-2xl fa fa-plus-circle"></i>
@@ -77,25 +107,25 @@ const AddProject = () => {
                         <div className="flex flex-wrap">
                             <div className="flex-none input-group">
                                 <div className="font-bold">Name</div>
-                                <input type="text" className="input-default w-60 sm:mr-5" />
+                                <input type="text" className="input-default w-60 sm:mr-5" value={keyPeopleLastName} onChange={(e) => setKeyPeopleLastName(e.target.value)} />
                             </div>
                             <div className="flex-none input-group">
                                 <div className="font-bold">Fast Name</div>
-                                <input type="text" className="input-default w-60 sm:mr-5" />
+                                <input type="text" className="input-default w-60 sm:mr-5" value={keyPeopleFirstName} onChange={(e) => setKeyPeopleFirstName(e.target.value)} />
                             </div>
                         </div>
                         <div className="flex flex-wrap">
                             <div className="flex-none input-group">
                                 <div className="font-bold">Telephone</div>
-                                <input type="text" className="input-default w-80 sm:mr-5" />
+                                <input type="text" className="input-default w-80 sm:mr-5" value={keyPeopleTelephone} onChange={(e) => setKeyPeopleTelephone(e.target.value)} />
                             </div>
                             <div className="flex-none input-group">
                                 <div className="font-bold">LinkedIn</div>
-                                <input type="text" className="input-default w-80 sm:mr-5" />
+                                <input type="text" className="input-default w-80 sm:mr-5" value={keyPeopleLinkedIn} onChange={(e) => setKeyPeopleLinkedIn(e.target.value)}/>
                             </div>
                         </div>
                         <div>
-                            <button className="py-2 text-white bg-black rounded-lg px-7" type="button">add</button>
+                            <button className="py-2 text-white bg-black rounded-lg px-7" type="button" onClick={() => { onAddToKeyPeoplesArr() }} >add</button>
                         </div>
                     </div>
                 </div>
@@ -103,7 +133,7 @@ const AddProject = () => {
 
             <div className="mt-5 how-long-age">
                 <h2 className="mb-2 text-2xl font-bold leading-normal">How long ago did you create your project?</h2>
-                <input type='text' className="input-default w-80 sm:w-96 sm:mr-5" />
+                <input type='number' className="input-default w-80 sm:w-96 sm:mr-5" />
             </div>
 
             <div className="mt-3 project-phase">
@@ -111,12 +141,6 @@ const AddProject = () => {
                 <div>
                     <button type="button" className="mx-1 mb-2 rounded-full btn-default">
                         Amorcage
-                    </button>
-                    <button type="button" className="mx-1 mb-2 rounded-full btn-default">
-                        Scaling
-                    </button>
-                    <button type="button" className="mx-1 mb-2 rounded-full btn-default">
-                        Epansion
                     </button>
                 </div>
             </div>
